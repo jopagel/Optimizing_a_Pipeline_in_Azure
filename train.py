4c73bf9eaa3f4df680a8a9dd3f2ff0d142e36021
Johannes <johannes.pagel@atruvia.de>
2023-05-11 14:06:51 +0200

    apply black formatting

--- train.py
+++ train.py
@@ -1,21 +1,30 @@
 from sklearn.linear_model import LogisticRegression
 import argparse
-import os
 import numpy as np
-from sklearn.metrics import mean_squared_error
-import joblib
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import OneHotEncoder
 import pandas as pd
 from azureml.core.run import Run
 from azureml.core import Workspace, Dataset
-from azureml.data.dataset_factory import TabularDatasetFactory
 import joblib
+
 
 def clean_data(data):
     # Dict for cleaning data
-    months = {"jan":1, "feb":2, "mar":3, "apr":4, "may":5, "jun":6, "jul":7, "aug":8, "sep":9, "oct":10, "nov":11, "dec":12}
-    weekdays = {"mon":1, "tue":2, "wed":3, "thu":4, "fri":5, "sat":6, "sun":7}
+    months = {
+        "jan": 1,
+        "feb": 2,
+        "mar": 3,
+        "apr": 4,
+        "may": 5,
+        "jun": 6,
+        "jul": 7,
+        "aug": 8,
+        "sep": 9,
+        "oct": 10,
+        "nov": 11,
+        "dec": 12,
+    }
+    weekdays = {"mon": 1, "tue": 2, "wed": 3, "thu": 4, "fri": 5, "sat": 6, "sun": 7}
 
     # Clean and one hot encode data
     x_df = data.to_pandas_dataframe().dropna()
@@ -44,8 +53,18 @@ def main():
     # Add arguments to script
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('--C', type=float, default=1.0, help="Inverse of regularization strength. Smaller values cause stronger regularization")
-    parser.add_argument('--max_iter', type=int, default=100, help="Maximum number of iterations to converge")
+    parser.add_argument(
+        "--C",
+        type=float,
+        default=1.0,
+        help="Inverse of regularization strength. Smaller values cause stronger regularization",
+    )
+    parser.add_argument(
+        "--max_iter",
+        type=int,
+        default=100,
+        help="Maximum number of iterations to converge",
+    )
 
     args = parser.parse_args()
 
@@ -54,22 +73,17 @@ def main():
     run.log("Regularization Strength:", np.float(args.C))
     run.log("Max iterations:", np.int(args.max_iter))
 
-
-    # TODO: Create TabularDataset using TabularDatasetFactory
-    # Data is located at:
-    # "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
-
     # define the URL of the CSV file
-    url = 'https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv'
+    url = "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
 
     # create a TabularDatasetFactory object and use it to create a TabularDataset
     dataset = Dataset.Tabular.from_delimited_files(path=url)
-    
+
     x, y = clean_data(dataset)
 
-    # TODO: Split data into train and test sets.
-
-    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.1, random_state = 42)
+    x_train, x_test, y_train, y_test = train_test_split(
+        x, y, test_size=0.1, random_state=42
+    )
 
     model = LogisticRegression(C=args.C, max_iter=args.max_iter).fit(x_train, y_train)
 
@@ -81,5 +95,6 @@ def main():
 
     run.upload_file(model_file_name, model_file_name)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
